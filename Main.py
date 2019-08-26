970b2a3c2167da46363904beb3eece4d2b336734
Bathiya Seneviratne <seneviratne.bathiya@gmail.com>
2019-08-26 17:42:28 +0530

    add seperate method for warning system

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import faceDetector as fd
 from playsound import playsound
+import warning
 
 cap = cv2.VideoCapture(0)
 
@@ -16,7 +17,7 @@ while(True):
     if len(facecount) > 3:
         faceinside = True
         if faceinside == True:
-            playsound('warning.mp3')
+            warning.warningSound()
 
     for(x,y,w,h) in facecount:
         cv2.rectangle(frame,(x,y),(x+w, y+h),(0,0,255),thickness=2)
---
+++ warning.py
@@ -0,0 +1,4 @@
+from playsound import playsound
+
+def warningSound():
+    playsound('warning.mp3')
